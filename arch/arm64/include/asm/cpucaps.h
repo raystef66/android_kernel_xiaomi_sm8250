13839319efdc69b4aa5661afbdc4ce31aed08e6c
¨raystef66¨ <s.vanbarel@gmail.com>
2022-04-25 19:20:27 +0200

    fix some merge derp in cpucaps.h to buil again...

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -57,14 +57,9 @@
 #define ARM64_WORKAROUND_1188873		36
 #define ARM64_WORKAROUND_1542418		37
 #define ARM64_WORKAROUND_1542419		38
+#define ARM64_SPECTRE_BHB               39
 
 /* kabi: reserve 39 - 63 for future cpu capabilities */
 #define ARM64_NCAPS				63
-#define ARM64_WORKAROUND_1463225		33
-#define ARM64_SSBS				34
-#define ARM64_WORKAROUND_1542419		35
-#define ARM64_SPECTRE_BHB			36
-
-#define ARM64_NCAPS				37
 
 #endif /* __ASM_CPUCAPS_H */
